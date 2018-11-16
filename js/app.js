c89837bf165da4778c563ff66328ac2d9b771a7f
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-11-16 12:14:02 -0600

    re-editting

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('hello');
+// // for ( var i = 0; i <= 16; i++ ) {
+// //   $('button').on('click', "calculator.answer.value +=" + )
+// // }
+// $(document).ready(function () {
+//   // i$(event.currentTarget.)
+//   console.log($input.val());
+// });
+$('.allClear').on('click', () => {
+  $('input').val('0');
+});
+
+$('.equals').on('click', () =>  {
+  let eq = $('input').val();
+  eq = eq.slice(0, eq.length -1);
+  eq = eval(eq);
+  $('input').val(eq);
+});
